46bcf6ffc9227f4a341fb6ffd590b0a604d2fe4b
dl <laughinglucky6@gmail.com>
2026-01-17 09:31:28 +0800

    use rendor.markdown to rendor, obsidian to write

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,36 +1,39 @@
 return {
-    {
-        "MeanderingProgrammer/render-markdown.nvim",
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
 
-        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-    },
-    config = function()
-        require("render-markdown").setup({
-            checkbox = {enabled = true},
-        indent={enabled = true}
-        })
-    end,
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	},
+	config = function()
+		-- require("render-markdown").setup({
+		-- 	checkbox = { enabled = false },
+		-- 	indent = { enabled = true },
+		-- })
+	end,
 
-    {
-        "epwalsh/obsidian.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "hrsh7th/nvim-cmp",
-            "nvim-telescope/telescope.nvim",
-            "nvim-treesitter",
-        },
-        config = function()
-            require("obsidian").setup({
-                workspaces = {
-                    {
-                        name = "dl note",
-                        path = "/Users/dl/obsidian/dl note",
-                    },
-                },
-                follow_url_func = function(url)
-                    vim.fn.jobstart({ "open", url }) -- Mac OS
-                end,
-            })
-        end,
-    },
+	{
+		"epwalsh/obsidian.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+			"nvim-telescope/telescope.nvim",
+			"nvim-treesitter",
+		},
+		config = function()
+			require("obsidian").setup({
+				workspaces = {
+					{
+						name = "dl note",
+						path = "/Users/dl/obsidian/dl note",
+					},
+				},
+				ui = {
+					enable = false, -- Obsidian handles checkboxes correctly
+				},
+				follow_url_func = function(url)
+					vim.fn.jobstart({ "open", url }) -- Mac OS
+				end,
+			})
+		end,
+	},
 }
