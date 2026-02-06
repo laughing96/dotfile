a1761b59572fbb0acb18440c4c8f35759079baff
dl <laughinglucky6@gmail.com>
2026-02-06 20:17:19 +0800

    feat:nvim vue config

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -62,6 +62,84 @@ return {
 			}
 			vim.lsp.enable("harper_ls")
 
+			-- vim.lsp.config["vue_ls"] = {
+			--     filetypes = { "vue" },
+			--     init_options = {
+			--         typescript = {
+			--             tsdk = vim.fn.expand(
+			--                 vim.fn.stdpath("data") ..
+			--                 "/mason/packages/typescript-language-server/node_modules/typescript/lib"
+			--             ),
+			--         },
+			--         vue = { hybridMode = false },
+			--     },
+			-- }
+			-- vim.lsp.enable("vue_ls")
+			-- vim.lsp.enable("ts_ls")
+
+			-- managed to get vue-language-server working with tsserver following https://github.com/vuejs/language-tools/wiki/Neovim
+			local vue_language_server_path = vim.fn.stdpath("data")
+				.. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+
+			local vue_plugin = {
+				name = "@vue/typescript-plugin",
+				location = vue_language_server_path,
+				languages = { "vue" },
+				configNamespace = "typescript",
+			}
+			local tsserver_config = {
+				settings = {
+					tsserver = {
+						tsserver = {
+							globalPlugins = {
+								vue_plugin,
+							},
+						},
+					},
+				},
+				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+			}
+
+			local vue_ls_config = {
+				on_init = function(client)
+					client.handlers["tsserver/request"] = function(_, result, context)
+						local clients = vim.lsp.get_clients({ bufnr = context.bufnr })
+
+						for _, c in ipairs(clients) do
+							print(c.name, c.config.cmd[1])
+						end
+						local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "tsserver" })
+
+						if #clients == 0 then
+							vim.notify(
+								"Could not found `tsserver` lsp client, vue_lsp would not work without it.",
+								vim.log.levels.ERROR
+							)
+							return
+						end
+						local ts_client = clients[1]
+
+						local param = unpack(result)
+						local id, command, payload = unpack(param)
+						ts_client:exec_cmd({
+							title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+							command = "typescript.tsserverRequest",
+							arguments = {
+								command,
+								payload,
+							},
+						}, { bufnr = context.bufnr }, function(_, r)
+							local response_data = { { id, r.body } }
+							---@diagnostic disable-next-line: param-type-mismatch
+							client:notify("tsserver/response", response_data)
+						end)
+					end
+				end,
+			}
+			-- nvim 0.11 or above
+			vim.lsp.config("tsserver", tsserver_config)
+			vim.lsp.config("vue_ls", vue_ls_config)
+			vim.lsp.enable({ "tsserver", "vue_ls" })
 			vim.lsp.config("clangd", {
 				cmd = { "clangd", "--background-index" },
 				filetypes = { "c", "cpp", "objc", "objcpp" },
