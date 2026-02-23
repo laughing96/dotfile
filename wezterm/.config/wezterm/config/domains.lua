ab78e9d2056430902aff72da6fd4035d689dfaf3
dl <laughinglucky6@gmail.com>
2026-02-23 20:35:08 +0800

    feat wezterm add ssh domain

--- wezterm/.config/wezterm/config/domains.lua
+++ wezterm/.config/wezterm/config/domains.lua
@@ -2,7 +2,17 @@ local platform = require('utils.platform')
 
 local options = {
    -- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
-   ssh_domains = {},
+   ssh_domains = {
+      {
+         name = 'linuxAsus',
+         remote_address = '192.168.0.106',
+         username = 'iron',
+         multiplexing = 'None',
+         -- default_prog = {'/bin/zsh && tmux','new-session','-As', 'main' },
+         default_prog = { '/bin/sh', '-c', '/bin/zsh && source ~/.zshrc && tmux new-session -As main' },
+         -- assume_shell='Posix',
+      },
+   },
 
    -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
    unix_domains = {},
