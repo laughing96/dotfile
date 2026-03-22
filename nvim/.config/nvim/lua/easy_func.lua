7d7933bb49d176d9e095c24982622ec652a3a7bf
dl <laughinglucky6@gmail.com>
2026-03-22 16:57:07 +0800

    other commet

--- nvim/.config/nvim/lua/easy_func.lua
+++ nvim/.config/nvim/lua/easy_func.lua
@@ -1,22 +1,10 @@
--- 格式化 JSON
-local function json_format()
-	fix_invalid_json()
-	vim.cmd("%!jq .")
-end
-
--- 压缩 JSON（变一行）
-local function json_minify()
-	fix_invalid_json()
-	vim.cmd("%!jq -c .")
-end
-
--- 只格式化选中的 JSON
-local function json_format_selection()
-	vim.cmd("'<,'>!jq .")
-end
-
 -- 修复常见非法 JSON
 local function fix_invalid_json()
+	-- 先保存原内容
+	local original = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+	local content = table.concat(original, "\n")
+	-- vim.notify("当前文件行数: " .. #original)
+	-- vim.notify(content:sub(1, 200))
 	vim.cmd([[
     %s/\<nan\>/null/gie
     %s/\<none\>/null/gie
@@ -25,6 +13,19 @@ local function fix_invalid_json()
     %s/'/"/ge
     %!jq .
   ]])
+	-- 再尝试用 jq 格式化
+	local ok2 = pcall(function()
+		vim.cmd("%!jq .")
+	end)
+    -- vim.notify(ok2)
+	if not ok2 then
+		-- jq 失败就恢复原内容
+		vim.api.nvim_buf_set_lines(0, 0, -1, false, original)
+		vim.notify("jq 解析失败，已恢复原文件", vim.log.levels.ERROR)
+		return
+	end
+
+	-- vim.notify("JSON 修复完成", vim.log.levels.INFO)
 end
 
 vim.keymap.set("n", "<leader>dab", ":g/^$/d<CR>", { desc = "Delete empty lines" })
